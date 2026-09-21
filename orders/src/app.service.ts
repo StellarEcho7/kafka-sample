f19a22b30d40b92440e9fcf6c2578352162919e6
Artem Petrenko <artem7888@gmail.com>
2026-09-21 20:13:53 +0300

    git and deps fixes

---
+++ orders/src/app.service.ts
@@ -0,0 +1,52 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { randomUUID } from 'node:crypto';
+import { OrderCreatedEvent } from '../../libs/kafka/events/order-created.event';
+import { ORDER_CREATED_TOPIC } from '../../libs/kafka/kafka.constants';
+
+@Injectable()
+export class AppService {
+  constructor(
+    @Inject('KAFKA_CLIENT')
+    private readonly kafka: ClientKafka,
+  ) {}
+
+  getOrder() {
+    return {
+      id: randomUUID(),
+      userId: 'user-123',
+      amount: 100,
+      currency: 'USD',
+    };
+  }
+
+  createOrder() {
+    // This should come from outside
+    const order = this.getOrder();
+
+    // creating an order...
+
+    // and this must be moved somewhere in a separate module
+    // and only call the method of the service
+    const event: OrderCreatedEvent = {
+      eventId: randomUUID(),
+      eventType: ORDER_CREATED_TOPIC,
+      occurredAt: new Date().toISOString(),
+
+      data: {
+        orderId: order.id,
+        userId: order.userId,
+        amount: order.amount,
+        currency: order.currency,
+      },
+    };
+
+    this.kafka.emit(ORDER_CREATED_TOPIC, event);
+
+    return order;
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
